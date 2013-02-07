bcdf13cdc73c23f403ec719b4e687a4a98bf4ec9
Edgar Buenrostro <goodface87@gmail.com>
2013-02-06 21:14:40 -0800

    Adds an AppView component for body

--- MyNeckMyBack/MyNeckMyBack/Scripts/buddies.js
+++ MyNeckMyBack/MyNeckMyBack/Scripts/buddies.js
@@ -1,1 +1,13 @@
-﻿
+﻿(function ($) {
+    window.AppView = Backbone.View.extend({
+        el: $("body"),
+        events: {
+            "click #add-friend": "showPrompt",
+        },
+        showPrompt: function () {
+            var friend_name = prompt("Who is your friend?");
+        }
+    });
+    var appview = new AppView;
+})(jQuery);
+
