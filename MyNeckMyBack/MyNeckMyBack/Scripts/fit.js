0bb598cff700a8c16e1d20b31180a440b3424b1d
Edgar Buenrostro <goodface87@gmail.com>
2013-02-09 14:36:33 -0800

    Adds a button, emtpy ul, and an event to the view
    The event is a click event for #add-card button that just alerts "you
    clicked"
    Very creative.

--- MyNeckMyBack/MyNeckMyBack/Scripts/fit.js
+++ MyNeckMyBack/MyNeckMyBack/Scripts/fit.js
@@ -77,43 +77,41 @@ $(function() {
             this.friends = new FlashCardCollection(null, { view: this });
             //Create a flash card collection when the view is initialized.
             //Pass it a reference to this view to create a connection between the two
+        },
+        events: {
+            "click #add-card":"showPrompt"
+        },
+        showPrompt: function() {
+            alert('you clicked');
+        }
+    });
+    var flashCardView = new FlashCardView;
+
+
+
+
+    window.AppView = Backbone.View.extend({
+        el: $("body"),
+        initialize: function () {
+            this.friends = new Friends(null, { view: this });
+            //Create a friends collection when the view is initialized.
+            //Pass it a reference to this view to create a connection between the two
+        },
+        events: {
+            "click #add-friend": "showPrompt",
+        },
+        showPrompt: function () {
+            var friend_name = prompt("Who is your friend?");
+            var friend_model = new Friend({ name: friend_name });
+            //Add a new friend model to our friend collection
+            this.friends.add(friend_model);
+        },
+        addFriendLi: function (model) {
+            //The parameter passed is a reference to the model that was added
+            $("#friends-list").append("<li>" + model.get('name') + "</li>");
+            //Use .get to receive attributes of the model
         }
 
     });
 
-
-
-
-
-
-
-
-
-
-
-
-    //window.AppView = Backbone.View.extend({
-    //    el: $("body"),
-    //    initialize: function () {
-    //        this.friends = new Friends(null, { view: this });
-    //        //Create a friends collection when the view is initialized.
-    //        //Pass it a reference to this view to create a connection between the two
-    //    },
-    //    events: {
-    //        "click #add-friend": "showPrompt",
-    //    },
-    //    showPrompt: function () {
-    //        var friend_name = prompt("Who is your friend?");
-    //        var friend_model = new Friend({ name: friend_name });
-    //        //Add a new friend model to our friend collection
-    //        this.friends.add(friend_model);
-    //    },
-    //    addFriendLi: function (model) {
-    //        //The parameter passed is a reference to the model that was added
-    //        $("#friends-list").append("<li>" + model.get('name') + "</li>");
-    //        //Use .get to receive attributes of the model
-    //    }
-
-    //});
-
-});
+});//End of document ready
