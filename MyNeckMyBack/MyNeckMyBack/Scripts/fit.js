2862c4daf266ae78b2bfdf74fe9c1fda8515b6e6
Edgar Buenrostro <goodface87@gmail.com>
2013-02-09 14:31:12 -0800

    Adds a view to the javascript
    This is a Backbone view called FlashCardView

--- MyNeckMyBack/MyNeckMyBack/Scripts/fit.js
+++ MyNeckMyBack/MyNeckMyBack/Scripts/fit.js
@@ -62,12 +62,58 @@ $(function() {
         name: null
     });
 
-    FlashCards = Backbone.Collection.extend({
+    FlashCardCollection = Backbone.Collection.extend({
+        model: FlashCard,       //Indicates what it's a collection of
         //This is our Friends collection and holds our Friend models
         initialize: function (models, options) {
             //this.bind("add", options.view.addFriendLi);
             //Listen for new additions to the collection and call a view function if so
         }
     });
+
+    window.FlashCardView = Backbone.View.extend({
+        el: $("body"),              //The element representing the view.
+        initialize: function() {    //Like a constructor
+            this.friends = new FlashCardCollection(null, { view: this });
+            //Create a flash card collection when the view is initialized.
+            //Pass it a reference to this view to create a connection between the two
+        }
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+    //window.AppView = Backbone.View.extend({
+    //    el: $("body"),
+    //    initialize: function () {
+    //        this.friends = new Friends(null, { view: this });
+    //        //Create a friends collection when the view is initialized.
+    //        //Pass it a reference to this view to create a connection between the two
+    //    },
+    //    events: {
+    //        "click #add-friend": "showPrompt",
+    //    },
+    //    showPrompt: function () {
+    //        var friend_name = prompt("Who is your friend?");
+    //        var friend_model = new Friend({ name: friend_name });
+    //        //Add a new friend model to our friend collection
+    //        this.friends.add(friend_model);
+    //    },
+    //    addFriendLi: function (model) {
+    //        //The parameter passed is a reference to the model that was added
+    //        $("#friends-list").append("<li>" + model.get('name') + "</li>");
+    //        //Use .get to receive attributes of the model
+    //    }
+
+    //});
 
 });
