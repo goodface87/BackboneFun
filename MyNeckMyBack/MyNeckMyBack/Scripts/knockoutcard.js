9e5c25bcf8759455e5d8a9dbe12c8787799a4bf7
Edgar Buenrostro <goodface87@gmail.com>
2013-02-12 19:54:50 -0800

    Adds a card object

--- MyNeckMyBack/MyNeckMyBack/Scripts/knockoutcard.js
+++ MyNeckMyBack/MyNeckMyBack/Scripts/knockoutcard.js
@@ -1,1 +1,9 @@
 ﻿//knockoutcard.js
+//Created by Edgar Ivan Buenrostro some time in early 2013
+
+// represent a single card item with a question and answer.
+var Card = function(question, answer) {
+    this.question = ko.observable(question);
+    this.answer = ko.observable(answer);
+    this.editing = ko.observable(false);
+}
