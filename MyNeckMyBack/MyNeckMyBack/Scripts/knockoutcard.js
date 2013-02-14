13557dc2c536bd05a3d8553767ea7cf8cf96e7fb
Edgar Buenrostro <goodface87@gmail.com>
2013-02-13 19:04:59 -0800

    Adds a ko viewmodel and an observable collection
    ...that happens tob e bound to the accordion list.

--- MyNeckMyBack/MyNeckMyBack/Scripts/knockoutcard.js
+++ MyNeckMyBack/MyNeckMyBack/Scripts/knockoutcard.js
@@ -6,4 +6,17 @@ var Card = function(question, answer) {
     this.question = ko.observable(question);
     this.answer = ko.observable(answer);
     this.editing = ko.observable(false);
-}
+};
+
+var ViewModel = function(todos) {
+    var self = this;
+
+    self.Cards = ko.observableArray([
+        new Card("8x9", "72"),
+        new Card("11x6", "66"),
+        new Card("12x12", "144")
+    ]);
+
+};
+
+ko.applyBindings(new ViewModel());
