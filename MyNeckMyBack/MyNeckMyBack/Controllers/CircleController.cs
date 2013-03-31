c630fd6b3246a6f529d58b45092f697bcbe544bf
Edgar Buenrostro <goodface87@gmail.com>
2013-03-30 22:17:29 -0700

    Adds Circle controller.

---
+++ MyNeckMyBack/MyNeckMyBack/Controllers/CircleController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyNeckMyBack.Controllers
+{
+    public class CircleController : Controller
+    {
+        //
+        // GET: /Circle/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
